06f50834bd591944f0c44ec64f49d5a9128f16e3
JuanElorriaga <juanelorriagadeoliveira@gmail.com>
2024-07-29 22:04:51 -0300

    criação do drop itens ao derrotar inimigos

--- Tiny Swords/Tiny Swords (Update 010)/enemies/enemy.gd
+++ Tiny Swords/Tiny Swords (Update 010)/enemies/enemy.gd
@@ -1,11 +1,17 @@
 class_name Enemy
 extends Node2D
-
+@export_category("Life")
 @export var health: int = 10
 @export var death_prefab: PackedScene
 var Damage_Digit_Prefab: PackedScene
 
 @onready var damage_digit_marker = $DamageDigitMarker
+
+@export_category("Drops")
+@export var drop_chance: float = 0.1
+@export var drop_itens: Array[PackedScene]
+@export var drop_chances: Array[float]
+
 
 func _ready():
 	Damage_Digit_Prefab = preload("res://Tiny Swords/Tiny Swords (Update 010)/misc/Damage_Digit.tscn")
@@ -36,4 +42,38 @@ func die() -> void:
 		death_object.position = position
 		get_parent().add_child(death_object)
 		
+		if randf() <= drop_chance:
+			drop_item()
+		
 		queue_free()
+
+func drop_item() -> void:
+	var drop = get_random_drop_item().instantiate()
+	drop.position = position
+	get_parent().add_child(drop)
+
+func get_random_drop_item() -> PackedScene:
+	# Listas com 1 item
+	if drop_itens.size() == 1:
+		return drop_itens[0]
+
+	# Calcular chance máxima
+	var max_chance: float = 0.0
+	for drop_chance in drop_chances:
+		max_chance += drop_chance
+	
+	# Jogar dado
+	var random_value = randf() * max_chance
+	
+	# Girar a roleta
+	var needle: float = 0.0
+	for i in drop_itens.size():
+		var drop_item = drop_itens[i]
+		var drop_chance = drop_chances[i] if i < drop_chances.size() else 1
+		if random_value <= drop_chance + needle:
+			return drop_item
+		needle += drop_chance
+	
+	return drop_itens[0]
+	
+	
